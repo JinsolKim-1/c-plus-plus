c7d7cd6dd6bdcaa4840ef011170decd5fc5e717f
JinsolKim-1 <98865655+JinsolKim-1@users.noreply.github.com>
2022-07-07 11:48:34 +0800

    Add files via upload

---
+++ average calculator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, e;
+    
+    cout<< "enter five numbers: ";
+    cin>>a>>b>>c>>d>>e;
+
+    int average= (a+b+c+d+e)/5;
+    cout<<average << endl;
+    system("pause");
+
+    return 0;
+
+}
